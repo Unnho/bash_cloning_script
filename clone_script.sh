8f0cee0889782dc45a46fa5fdb15828690ec9369
Sajid Chowdhury <127777393+Unnho@users.noreply.github.com>
2024-06-11 12:11:24 +0530

    Update clone_script.sh

--- clone_script.sh
+++ clone_script.sh
@@ -1,1 +1,9 @@
-git clone https://github.com/TheMuppets/proprietary_vendor_motorola_sm6225-common vendor/motorola/sm6225-common -b lineage-21 && git clone https://github.com/TheMuppets/proprietary_vendor_motorola_devon vendor/motorola/devon -b lineage-21 && git clone https://github.com/LineageOS/android_hardware_motorola hardware/motorola -b lineage-21 && git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings && git clone https://github.com/LineageOS/android_system_qcom system/qcom && git clone https://github.com/LineageOS/android_kernel_motorola_sm6225 kernel/motorola/sm6225 -b lineage-21 && git clone https://github.com/LineageOS/android_device_motorola_devon device/motorola/devon -b lineage-21 && git clone https://github.com/LineageOS/android_device_motorola_sm6225-common device/motorola/sm6225-common -b lineage-21
+git clone https://github.com/TheMuppets/proprietary_vendor_motorola_sm6225-common vendor/motorola/sm6225-common -b lineage-21 && \
+git clone https://github.com/TheMuppets/proprietary_vendor_motorola_devon vendor/motorola/devon -b lineage-21 && \
+git clone https://github.com/LineageOS/android_hardware_motorola hardware/motorola -b lineage-21 && \
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings && \
+git clone https://github.com/LineageOS/android_system_qcom system/qcom && \
+git clone https://github.com/LineageOS/android_kernel_motorola_sm6225 kernel/motorola/sm6225 -b lineage-21 && \
+git clone https://github.com/LineageOS/android_device_motorola_devon device/motorola/devon -b lineage-21 && \
+git clone https://github.com/LineageOS/android_device_motorola_sm6225-common device/motorola/sm6225-common -b lineage-21
+echo "done"
